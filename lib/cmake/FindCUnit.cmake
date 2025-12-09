18a3690f5231c880ce8f5ef6e6e0b0c0f0ec00c3
TankManDu70 <thomas.perraton@gmail.com>
2025-12-09 10:06:54 +0100

    findcunit cmake config

--- lib/cmake/FindCUnit.cmake
+++ lib/cmake/FindCUnit.cmake
@@ -3,6 +3,6 @@ find_library(CUNIT_LIBRARIES NAMES CUnit cunit)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  CUnit]
+  CUnit
   REQUIRED_VARS CUNIT_INCLUDE_DIRS CUNIT_LIBRARIES
   VERSION_VAR CUNIT_VERSION)

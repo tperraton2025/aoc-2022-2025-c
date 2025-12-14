9ef8030b897a2c699bbec88481b88c739262be03
TankManDu70 <thomas.perraton@gmail.com>
2025-12-14 09:42:23 +0100

    re-arranged files

---
+++ 2025/004/partx.c
@@ -0,0 +1,63 @@
+#include "partx.h"
+
+struct parser blockparser = {._name = "roll parsers", ._func = parserolls};
+
+int parserolls(void *arg, char *str)
+{
+    int _ret = 0;
+    aoc_2d_eng_h eng = (aoc_2d_eng_h)arg;
+
+    char *_start = str;
+    _start = strchr(_start, '@');
+    while (_start && !_ret)
+    {
+        coord_t _pos = {._x = _start - str, ._y = engine_last_line(eng)};
+        if (_start)
+            _start++;
+
+        char _objname[] = "roll9999999";
+        sprintf(_objname, "roll%-3s", strobjcnt(eng));
+        aoc_2d_obj_h _objh = aoc_2d_obj_ctor(eng, _objname, &_pos, "@", OBJ_FLAG_STATIC, GREEN);
+        if (!_objh)
+            _ret = ENOMEM;
+        if (!_ret)
+            _ret = aoc_2d_eng_append_obj(eng, _objh);
+        _start = strchr(_start, '@');
+    }
+    engine_add_line(eng);
+    return _ret;
+}
+
+size_t trimaccessiblepositions(aoc_2d_eng_h eng, dll_head_h allpos)
+{
+    size_t liberated = 0;
+    size_t startlen = allpos->_size;
+    dll_node_t ghostnode = {0};
+    LL_FOREACH_P(_posn, allpos)
+    {
+        size_t _limit = 0;
+        coord_tracker_h _trkh = (coord_tracker_h)_posn;
+        coord_t *_posh = &_trkh->_coord;
+
+        _limit += dll_try_count_nodes_by_property(_posn, (void *)_posn, coord_dist_less_than_2, coord_dist_more_than_2);
+        if (4 > _limit)
+            _posn->_obsolete = true;
+    }
+    LL_FOREACH_P_EXT(_posn, allpos)
+    {
+        if (_posn->_obsolete)
+        {
+            liberated++;
+            coord_tracker_h _trkh = (coord_tracker_h)_posn;
+            coord_t *_posh = &_trkh->_coord;
+            aoc_2d_eng_draw_part_at(eng, _posh, "x", RED);
+            ghostnode._next = _posn->_next;
+            dll_node_disconnect(allpos, _posn);
+            free(_posn);
+            if (!ghostnode._next)
+                break;
+            _posn = &ghostnode;
+        }
+    }
+    return liberated;
+}
--- 2025/004/partx.h
+++ 2025/004/partx.h
@@ -15,68 +15,10 @@ struct context
     struct dll_head _parsers;
     size_t _result;
 };
-
-static int parserolls(void *arg, char *str)
-{
-    int _ret = 0;
-    aoc_2d_eng_h eng = (aoc_2d_eng_h)arg;
-
-    char *_start = str;
-    _start = strchr(_start, '@');
-    while (_start && !_ret)
-    {
-        coord_t _pos = {._x = _start - str, ._y = engine_last_line(eng)};
-        if (_start)
-            _start++;
-
-        char _objname[] = "roll9999999";
-        sprintf(_objname, "roll%-3s", strobjcnt(eng));
-        aoc_2d_obj_h _objh = aoc_2d_obj_ctor(eng, _objname, &_pos, "@", OBJ_PROPERTY_STATIC, GREEN);
-        if (!_objh)
-            _ret = ENOMEM;
-        if (!_ret)
-            _ret = aoc_2d_eng_append_obj(eng, _objh);
-        _start = strchr(_start, '@');
-    }
-    engine_add_line(eng);
-    return _ret;
-}
-
-static struct parser blockparser = {._name = "roll parsers", ._func = parserolls};
-
-
-static size_t trimaccessiblepositions(aoc_2d_eng_h eng, dll_head_h allpos)
-{
-    size_t liberated = 0;
-    size_t startlen = allpos->_size;
-    LL_FOREACH_P(_posn, allpos)
-    {
-        size_t _limit = 0;
-        coord_tracker_h _trkh = (coord_tracker_h)_posn;
-        coord_t *_posh = &_trkh->_coord;
-
-        _limit += dll_try_count_nodes_by_property(_posn, (void *)_posn, coord_dist_less_than_2, coord_dist_more_than_2);
-        if (4 > _limit)
-            _posn->_obsolete = true;
-    }
-    dll_node_t _dummy = {0};
-    LL_FOREACH_P_EXT(_posn, allpos)
-    {
-        if (_posn->_obsolete)
-        {
-            liberated++;
-            coord_tracker_h _trkh = (coord_tracker_h)_posn;
-            coord_t *_posh = &_trkh->_coord;
-            aoc_2d_eng_draw_part_at(eng, _posh, "x", RED);
-            _dummy._next = _posn->_next;
-            dll_node_disconnect(allpos, _posn);
-            free(_posn);
-            if (!_dummy._next)
-                break;
-            _posn = &_dummy;
-        }
-    }
-    return liberated;
-}
 typedef struct context *aoc_context_h;
-static void markfreerolls(struct context *_ctx);
+
+int parserolls(void *arg, char *str);
+void markfreerolls(struct context *_ctx);
+size_t trimaccessiblepositions(aoc_2d_eng_h eng, dll_head_h allpos);
+
+extern struct parser blockparser;
